8f0587dbbdf09a21164e7c8f79cecba79c843382
Lachie King <kingy00112233@gmail.com>
2025-12-11 04:40:24 +0000

    add README, add task 0 function file, add bigOh file and add the header file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in asc order
+ * @array: array to sort
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t count;
+	int temp;
+	bool check = false;
+
+	if (!array || size < 2)
+		return;
+
+	while (!check)
+	{
+		check = true;
+		for (count = 0; count < size - 1; count++)
+		{
+			if (array[count] > array[count + 1])
+				{
+					temp = array[count];
+					array[count] = array[count + 1];
+					array[count + 1] = temp;
+					check = false;
+				}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+This README outlines what happens in each file of the Sorting Algorithms GIT
+
+0-bubble_sort.c      - sorts an array of integers using the bubble sort algorithm
+
