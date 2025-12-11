c61bee7034eb67e1b9a3f437c17d3e9b381199a5
Lachie King <kingy00112233@gmail.com>
2025-12-11 05:04:07 +0000

    just adding for future work

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list in asc order, using insert alg
+ * @list: pointer to a pointer of a list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
