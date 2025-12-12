eb557ae1d54cfd5a248a9f6774647d0928d7c651
Lachie King <kingy00112233@gmail.com>
2025-12-12 01:14:13 +0000

    add docs for task 1 and update header and README file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,5 +7,38 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *node, *temp;
 
+	if (!list || !*list)
+		return;
+
+	node = (*list)->next;
+
+
+
+	while (node)
+	{
+		temp = node;
+
+		while (temp->prev && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+
+			if (temp->next)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+
+			temp->next->prev = temp;
+
+			if (temp->prev)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+
+			print_list(*list);
+		}
+		node = node->next;
+	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 This README outlines what happens in each file of the Sorting Algorithms GIT
 
-0-bubble_sort.c      - sorts an array of integers using the bubble sort algorithm
+0-bubble_sort.c	     	  	  - sorts an array of integers using the bubble sort algorithm
 
+1-insertion_sort_list		  - sorts a doubly linked list of integers in ascending order
